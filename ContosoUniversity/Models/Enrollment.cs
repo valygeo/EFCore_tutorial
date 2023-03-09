299383c6cb3b8d50749b39f4905ed98dc3bf2233
Vali <94057367+valygeo@users.noreply.github.com>
2023-03-09 10:06:37 +0200

    -complex data model done

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,14 +1,21 @@
-﻿namespace ContosoUniversity.Models { 
-public enum Grade
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
 {
-    A,B,C,D,F
-}
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
     public class Enrollment
     {
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
+
         public Course Course { get; set; }
         public Student Student { get; set; }
     }
